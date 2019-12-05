8f09325a8ef9f4b32da76215fa14920e990c3f0b
ananyach9 <ananyach26@gmail.com>
2019-12-05 18:18:04 +0530

    Added routing

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
+import { Link } from 'react-router-dom';
+
 
 class AddSusbscriber extends Component {
     constructor() {
@@ -22,6 +24,7 @@ class AddSusbscriber extends Component {
         e.preventDefault();
         this.props.addSubscriberHandler(this.state);
         this.setState({ id: 0, name: '', phone: ' ' });
+        this.props.history.push("/");
     }
     render() {
         const { name, phone } = this.state;
@@ -29,7 +32,9 @@ class AddSusbscriber extends Component {
             <div>
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
-                    <button className="custom-btn">Back</button>
+                <Link to="/">
+                        <button className="custom-btn">Back</button>
+                    </Link>
 
                     <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label><br />
